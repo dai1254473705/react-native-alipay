b5712b5595ef69ab8bd824ace54322ba5f46de08
代云舟 <1254473705@qq.com>
2018-07-07 14:16:35 +0800

    添加按钮，安装react-native-yunpeng-alipay

--- App.js
+++ App.js
@@ -7,43 +7,62 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View
+} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
 export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+    _aliPay = () => {
+        console.log("点击进行支付");
+
+
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <TouchableOpacity onPress={this._aliPay}>
+                    <View style={styles.btn}>
+                        <Text style={styles.text}>支付宝支付</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+    btn: {
+        width: 200,
+        height: 30,
+        backgroundColor: "#999",
+
+
+    },
+    text: {
+        color: "#fff",
+        textAlign: "center",
+        lineHeight: 30
+    }
 });
